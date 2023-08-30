eae94fa84d420d0ad3b0884c7be9941c1a365a4c
Mattia Mezzone <mattia.mezzone@ericsson.com>
2023-08-30 17:54:44 +0200

    830

--- EX4 - Digital Signal Processing/Main.py
+++ EX4 - Digital Signal Processing/Main.py
@@ -1,36 +1,64 @@
 # Signal generation
 
+import json
 import numpy as np
 import scipy as sp
-from math import pi,sin
+from math import pi,sin,cos,e,sqrt
 import matplotlib.pyplot as plt
 import control as ct
 
-t = np.linspace(0,1e-4,5000)
-y = [np.random.rand() for tt in t]
+# t = np.linspace(0,1e-4,50000)
+# y = [np.random.rand() for tt in t]
 
 
 # plt.plot(sp.fft.fft(y))
+f = open('Trace_Y_After_FIR.json')
+y = json.load(f)['Trace_Y_After_FIR']
 
-wm = 100*1e6
+f = 100*1e6
+wm = 2*pi*f
 Q = 200
 
-
-
-H = ct.tf([1e10/4*wm/Q],[1,wm/Q,wm**2])
-w = [2*pi*(i)*1e2 for i in range(1000000)]
-plt.figure(1)
-ct.bode_plot(H,omega = w)
+t = np.linspace(0,(len(y)/40)/f,len(y),False)
+y = [sin(wm*tt) for tt in t]
+# wm = wm - wm/1000
+# w = [2*pi*(i)*1e7 for i in range(10000)]
+# plt.figure(1)
+# ct.bode_plot(H,omega = w)
 plt.figure(2)
 plt.subplot(311)
 plt.plot(t,y)
-x,z = ct.forced_response(H,t,y)
-plt.subplot(312)
-plt.plot(x,z)
 plt.subplot(313)
-# plt.plot(sp.fft.fft(y))
-plt.plot(sp.fft.fft(z))
-z = [sin(2*pi*1e8*t[zz])*z[zz] for zz in range(len(z))]
-plt.plot(sp.fft.fft(z))
+zpI = [y[zz] * cos(wm*t[zz]) for zz in range(len(y))]
+zmQ = [y[zz] * - sin(wm*t[zz]) for zz in range(len(y))]
+wh = 2*pi*125000
+H = ct.tf([wh],[1,wh])
+tttp,filpI = ct.forced_response(H,t,zpI)
+tttm,filmQ = ct.forced_response(H,t,zmQ)
+# plt.plot(np.fft.fftfreq(len(t),d=t[1]-t[0]),abs(sp.fft.fft(y)))
+plt.plot(np.fft.fftfreq(len(filpI),d=tttp[1]-tttp[0]),abs(sp.fft.fft(filpI)))
+# plt.plot(np.fft.fftfreq(len(filmQ),d=tttm[1]-tttm[0]),abs(sp.fft.fft(filmQ)))
 plt.subplot(312)
-plt.plot(x,z)
+plt.plot(t,y)
+# plt.plot(t,zp)
+# plt.plot(tttp,filp)
+# plt.plot(t,zm)
+# plt.plot(tttm,film)
+
+newt,newpI,newmQ = [],[],[]
+for i in range(len(tttp)):
+    if i%1 == 0:
+        newt.append(tttp[i])
+        newpI.append(filpI[i])
+        newmQ.append(filmQ[i])
+
+# plt.plot(newt,newp,'x')
+# plt.plot(newt,newm,'x')
+
+# plt.subplot(313)
+# plt.plot(np.fft.fftfreq(len(newt),d=newt[1]-newt[0]),sp.fft.fft(newp))
+# plt.plot(np.fft.fftfreq(len(newt),d=newt[1]-newt[0]),sp.fft.fft(newm))
+
+zout = [sqrt(newpI[ti]**2 + newmQ[ti]**2) * cos(wm*newt[ti] + np.arctan(newmQ[ti]/newpI[ti])) for ti in range(len(newt))]
+plt.plot(newt,zout)
+
