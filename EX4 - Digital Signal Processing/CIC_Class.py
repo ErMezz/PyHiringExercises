bfa464c0b970a4c40f0c2c428cf0a56df5bf7892
Mattia Mezzone <mattia.mezzone@ericsson.com>
2023-09-07 15:57:45 +0200

    Downconverter

--- EX4 - Digital Signal Processing/CIC_Class.py
+++ EX4 - Digital Signal Processing/CIC_Class.py
@@ -8,6 +8,7 @@ import control as ct
 from math import sin,pi,cos,e
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import signal
 
 class integrator:
     def __init__(self):
@@ -34,22 +35,22 @@ class CIC:
         self.integ = [integrator()]*order
         self.comb = [comb()]*order
         self.data = 0
-        self.count = -2
+        self.count = -1
         self.decim = decim
     
     def update(self,inp):
-        self.data = inp
+        data = inp
         for integ in self.integ:
-            self.data = integ.update(self.data)
+            data = integ.update(data)
         self.count += 1
         if self.count % self.decim == 0:
             self.count = 0
             for comb in self.comb:
-                self.data = comb.update(self.data)
-            return self.data
+                data = comb.update(data)
+            return data
         else: return False
 
-class Simp_FIR:
+class Dig_FIL:
     def __init__(self,taps=[]):
         self.taps = taps
         self.val = []
@@ -77,26 +78,37 @@ decimation = 25
 N = 1
 gain = (decimation * 1) ** N
 
-A = -18
+A = 10 #CIC compensator taps
+B= 1-A #CIC compensator taps
+CG = A+A+B
+
+NN=32
+bands = [0., .22, .28, .5]
+h = signal.remez(NN+1, bands, [1,0], [1,1])
+h[abs(h) <= 1e-4] = 0.
+TG = sum(h)
 
 FIL_Q = CIC(decimation,N)
 FIL_P = CIC(decimation,N)
-FIR_Q = Simp_FIR([1,A,1])
-FIR_P = Simp_FIR([1,A,1])
+Q_CICComp = Dig_FIL([A,B,A])
+P_CICComp = Dig_FIL([A,B,A])
+Q_FIR = Dig_FIL(h)
+P_FIR = Dig_FIL(h)
 
 samples = 1e4+1
 dT = 1e-6
 
 t = [tt*dT for tt in range(int(samples))]
-undert = [tt*dT*decimation for tt in range(int(samples)//decimation)]
-undert.pop(0)
+undert = [tt*dT*decimation*2 for tt in range(int(samples)//(decimation*2))]
+
 y = [inp_samp(x) for x in t]
 yQ = [inp_samp(x)*e**(1j*2*pi*4000*x) for x in t]
 yP = [inp_samp(x)*e**(-1j*2*pi*4000*x) for x in t]
 # yQ = [inp_samp(x)*1j*sin(2*pi*4000*x) for x in t]
 # yP = [inp_samp(x)*cos(2*pi*4000*x) for x in t]
 # yQ = [1,2,3,4,5,6,7,8]
-z = np.fft.fft(y).tolist()
+z = np.fft.fft(y)
+z = (z/len(z)).tolist()
 z = z [len(z)//2:] + z[:len(z)//2]
 z = [zz.imag for zz in z]
 ff = np.fft.fftfreq(len(t),dT).tolist()
@@ -105,20 +117,30 @@ plt.plot(ff,z)
 
 rebuild_Q = []
 ss = []
+re = True
 for (s, v) in enumerate(yQ):
     up = FIL_Q.update(v)
-    if up != False: 
-        up = FIR_Q.update(up)
-        rebuild_Q.append(up/gain)
+    if type(up) != bool: 
+        up = Q_CICComp.update(up)/CG
+        up = Q_FIR.update(up)/TG
+        if re: 
+            rebuild_Q.append(up/gain)
+            re=False
+        else: re=True
         ss.append(s)
 
 rebuild_P = []
 pp=[]
+re = True
 for (s, v) in enumerate(yP):
     up = FIL_P.update(v)
-    if up != False: 
-        up = FIR_P.update(up)
-        rebuild_P.append(up/gain)
+    if type(up) != bool: 
+        up = P_CICComp.update(up)/CG
+        up = P_FIR.update(up)/TG
+        if re: 
+            rebuild_P.append(up/gain)
+            re=False
+        else: re=True
         pp.append(s)
 
 z = np.fft.fft(rebuild_Q).tolist()
@@ -137,10 +159,11 @@ ff = (ff[len(ff)//2:] + ff[:len(ff)//2])
 
 rebuild = [rebuild_Q[tt]*e**(-1j*2*pi*4000*undert[tt]) + rebuild_P[tt]*e**(1j*2*pi*4000*undert[tt]) for tt in range(len(undert))]
 # rebuild = [rebuild_Q[tt]*1j*sin(2*pi*4000*undert[tt]) + rebuild_P[tt]*cos(2*pi*4000*undert[tt]) for tt in range(len(undert))]
-z = np.fft.fft(rebuild).tolist()
+z = np.fft.fft(rebuild)
+z = (z/len(z)).tolist()
 z = z [len(z)//2:] + z[:len(z)//2]
 z = [zz.imag for zz in z]
-ff = np.fft.fftfreq(len(t)//decimation-1,dT*decimation).tolist()
+ff = np.fft.fftfreq(len(t)//(decimation*2),dT*decimation*2).tolist()
 ff = (ff[len(ff)//2:] + ff[:len(ff)//2])
 plt.plot(ff,z)
 
